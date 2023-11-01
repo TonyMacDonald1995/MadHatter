64c25f3c70931e3fe83aab942ebbbe4615e4bbe7
Tony MacDonald <tonymacdonald1995@gmail.com>
2023-11-01 07:14:48 -0500

    Added Nickname Shuffle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("net.dv8tion:JDA:5.0.0-beta.2")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
