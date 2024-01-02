64f801ed41474a899465408ef4950755292091a9
Tony MacDonald <tonymacdonald1995@gmail.com>
2024-01-02 00:59:47 -0600

    Added /nnself Added Check for shuffled state before changing nicknames Added Auto-restore at startup

--- src/main/kotlin/com/tonymacdonald1995/madhatter/MadHatter.kt
+++ src/main/kotlin/com/tonymacdonald1995/madhatter/MadHatter.kt
@@ -40,11 +40,17 @@ fun main(args : Array<String>) {
 
 class MadHatter : ListenerAdapter() {
 
+    private val shuffledMap = mutableMapOf<String, Boolean>()
+
     override fun onGuildReady(event : GuildReadyEvent) {
 
         log("Connected to " + event.guild.name)
 
         event.guild.selfMember.modifyNickname("Mad Hatter").queue()
+
+        event.guild.upsertCommand("nnme", "Changes your nickname")
+            .addOption(OptionType.STRING, "nickname", "New nickname", false)
+            .queue()
 
         event.guild.upsertCommand("nn", "Changes a user's nickname")
             .addOption(OptionType.USER, "user", "User to change nickname", true)
@@ -56,6 +62,11 @@ class MadHatter : ListenerAdapter() {
 
         event.guild.upsertCommand("nnrestore", "Restores nicknames from last backup, if one exists.")
             .queue()
+
+        shuffledMap[event.guild.id] = false
+        nicknameRestore(event)
+
+
     }
 
     override fun onSlashCommandInteraction(event : SlashCommandInteractionEvent) {
@@ -65,6 +76,7 @@ class MadHatter : ListenerAdapter() {
 
         when(event.name) {
             "nn" -> changeNickname(event)
+            "nnme" -> changeSelfNickname(event)
             "nnbackup" -> nicknameBackup(event)
             "nnrestore" -> nicknameRestore(event)
             else -> {
@@ -77,20 +89,28 @@ class MadHatter : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (event.message.member == event.guild.selfMember)
             return
+        if (shuffledMap[event.guild.id] == true)
+            return
+
         val triggerWords = listOf("change", "swap", "shift", "switch", "trade")
         if (event.message.contentDisplay.containsAny(triggerWords, false)) {
             nicknameShuffle(event.guild)
             event.channel.sendMessage("https://tenor.com/view/futurama-change-places-musical-chairs-gif-14252770").queue()
+            shuffledMap[event.guild.id] = true
         }
     }
 
     private fun changeNickname(event : SlashCommandInteractionEvent) {
         event.deferReply(true).queue()
-        val hook = event.hook
-        hook.setEphemeral(true)
+        val hook = event.hook.setEphemeral(true)
         val member = event.getOption("user")?.asMember ?: return
         val selfMember = event.guild?.selfMember ?: return
         val nickname = event.getOption("nickname")?.asString ?: ""
+
+        if (shuffledMap[event.guild!!.id] == true) {
+            hook.sendMessage("Nicknames cannot be changed while they are shuffled, to proceed use /nnrestore").queue()
+            return
+        }
 
         if (!selfMember.hasPermission(Permission.NICKNAME_MANAGE)) {
             hook.sendMessage("Error: I don't have permission to manage nicknames.").queue()
@@ -99,11 +119,6 @@ class MadHatter : ListenerAdapter() {
 
         if (member == selfMember) {
             hook.sendMessage("You cannot change my nickname").queue()
-            return
-        }
-
-        if (event.member?.hasPermission(Permission.NICKNAME_CHANGE) == false && event.member == member) {
-            hook.sendMessage("You don't have permission to change your nickname").queue()
             return
         }
 
@@ -118,82 +133,120 @@ class MadHatter : ListenerAdapter() {
         }
 
         member.modifyNickname(nickname).queue()
-        hook.sendMessage(member.user.name + "'s nickname has been changed to " + nickname).queue()
-        log(event.member?.user?.name + " changed " + member.user.name + "'s nickname to " + nickname)
-    }
-}
-
-private fun nicknameBackup(event: SlashCommandInteractionEvent) {
-    if (event.guild == null) {
-        event.reply("Error: Command must be called from a server.").setEphemeral(true).queue()
-        return
-    }
-    val guild = event.guild
-    event.deferReply(true).queue()
-    val hook = event.hook
-    val memberList = guild!!.members.filter {
-        it != guild.selfMember && guild.selfMember.canInteract(it)
-    }.toMutableList()
-
-    val nicknameMap = memberList.associate { it.idLong to (it.nickname ?: "") }.toMutableMap()
-    saveNicknameData(guild.id, nicknameMap)
-    hook.sendMessage("Backup made successfully.").queue()
-}
-
-private fun nicknameRestore(event: SlashCommandInteractionEvent) {
-    if (event.guild == null) {
-        event.reply("Error: Command must be called from a server.").setEphemeral(true).queue()
-        return
+        hook.sendMessage("${member.user.name}'s nickname has been changed to $nickname").queue()
+        log("${event.member?.user?.name} changed ${member.user.name}'s nickname to $nickname")
     }
 
-    val guild = event.guild
-    event.deferReply(true).queue()
-    val hook = event.hook
-    val nicknameMap = loadNicknameData(guild!!.id)
+    private fun changeSelfNickname(event: SlashCommandInteractionEvent) {
+        event.deferReply(true).queue()
+        val hook = event.hook.setEphemeral(true)
+        val selfMember = event.guild?.selfMember ?: return
+        val member = event.member ?: return
+        val nickname = event.getOption("nickname")?.asString ?: ""
 
-    if (nicknameMap.isEmpty()) {
-        hook.sendMessage("Error: Unable to restore backup.").queue()
-        return
-    }
+        if (shuffledMap[event.guild!!.id] == true) {
+            hook.sendMessage("Nicknames cannot be changed while they are shuffled, to proceed use /nnrestore").queue()
+            return
+        }
 
-    for(pair in nicknameMap)
-        guild.getMemberById(pair.key)?.modifyNickname(pair.value)?.queue()
-    hook.sendMessage("Backup restored successfully.").queue()
-}
+        if (selfMember.hasPermission(Permission.NICKNAME_MANAGE)) {
+            hook.sendMessage("Error: I don't have permission to manage nicknames.").queue()
+            return
+        }
 
-private fun nicknameShuffle(guild: Guild) {
-    val memberList = guild.members.filter {
-        it != guild.selfMember && guild.selfMember.canInteract(it)
-    }.toMutableList()
+        if (!selfMember.canInteract(member)) {
+            hook.sendMessage("You are too powerful for me to change your nickname.").queue()
+            return
+        }
 
-    val shuffledNicknames = memberList.mapNotNull { it.nickname ?: it.user.globalName }.shuffled()
+        if (nickname.length > 32) {
+            hook.sendMessage("That name is over the max length of 32 characters.").queue()
+            return
+        }
 
-    memberList.zip(shuffledNicknames).forEach { (member, nickname) ->
         member.modifyNickname(nickname).queue()
+        hook.sendMessage("Your nickname has been changed to $nickname").queue()
+        log("${member.user.name} has changed their nickname to $nickname.")
+
     }
 
+    private fun nicknameBackup(event: SlashCommandInteractionEvent) {
+        val guild = event.guild ?: return
+        val memberList = guild.members.filter {
+            it != guild.selfMember && guild.selfMember.canInteract(it)
+        }.toMutableList()
 
-}
-
-private fun loadNicknameData(guildId: String): MutableMap<Long, String> {
-    val gson = Gson()
-    val file = File("/data/$guildId.json")
-    return if (file.exists()) {
-        val json = file.readText()
-        val memberDataList = gson.fromJson(json, Array<NicknameData>::class.java)
-        val nicknameMap = mutableMapOf<Long, String>()
-        memberDataList.forEach { nicknameMap[it.id] = it.nickname }
-        nicknameMap
-    } else {
-        mutableMapOf()
+        val nicknameMap = memberList.associate { it.idLong to (it.nickname ?: "") }.toMutableMap()
+        saveNicknameData(guild.id, nicknameMap)
     }
-}
 
-private fun saveNicknameData(guildId: String, nicknameMap: MutableMap<Long, String>) {
-    val gson = Gson()
-    val memberDataList = nicknameMap.map { NicknameData(it.key, it.value) }
-    val json = gson.toJson(memberDataList)
-    File("/data/$guildId.json").writeText(json)
+    private fun nicknameRestore(event: GuildReadyEvent) {
+        val nicknameMap = loadNicknameData(event.guild.id)
+
+        if (nicknameMap.isEmpty())
+            return
+        for (pair in nicknameMap)
+            event.guild.getMemberById(pair.key)?.modifyNickname(pair.value)?.queue()
+        File("/data/${event.guild.id}.json").delete()
+    }
+
+    private fun nicknameRestore(event: SlashCommandInteractionEvent) {
+        if (event.guild == null) {
+            event.reply("Error: Command must be called from a server.").setEphemeral(true).queue()
+            return
+        }
+
+        val guild = event.guild ?: return
+        event.deferReply(true).queue()
+        val hook = event.hook
+        val nicknameMap = loadNicknameData(guild.id)
+
+        if (nicknameMap.isEmpty()) {
+            hook.sendMessage("Error: Unable to restore backup.").queue()
+            return
+        }
+
+        for(pair in nicknameMap)
+            guild.getMemberById(pair.key)?.modifyNickname(pair.value)?.queue()
+        hook.sendMessage("Backup restored successfully.").queue()
+        File("/data/${guild.id}.json").delete()
+        shuffledMap[guild.id] = false
+    }
+
+    private fun nicknameShuffle(guild: Guild) {
+        val memberList = guild.members.filter {
+            it != guild.selfMember && guild.selfMember.canInteract(it)
+        }.toMutableList()
+
+        val shuffledNicknames = memberList.mapNotNull { it.nickname ?: it.user.globalName }.shuffled()
+
+        memberList.zip(shuffledNicknames).forEach { (member, nickname) ->
+            member.modifyNickname(nickname).queue()
+        }
+
+
+    }
+
+    private fun loadNicknameData(guildId: String): MutableMap<Long, String> {
+        val gson = Gson()
+        val file = File("/data/$guildId.json")
+        return if (file.exists()) {
+            val json = file.readText()
+            val memberDataList = gson.fromJson(json, Array<NicknameData>::class.java)
+            val nicknameMap = mutableMapOf<Long, String>()
+            memberDataList.forEach { nicknameMap[it.id] = it.nickname }
+            nicknameMap
+        } else {
+            mutableMapOf()
+        }
+    }
+
+    private fun saveNicknameData(guildId: String, nicknameMap: MutableMap<Long, String>) {
+        val gson = Gson()
+        val memberDataList = nicknameMap.map { NicknameData(it.key, it.value) }
+        val json = gson.toJson(memberDataList)
+        File("/data/$guildId.json").writeText(json)
+    }
 }
 
 fun String.containsAny(list: List<String>, caseSensitive: Boolean = true): Boolean {
